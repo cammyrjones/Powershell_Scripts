ca6541b2385f6de64bd66a4f33c3088e19016915
Cameron Jones <cjones2333@gmail.com>
2019-02-11 16:25:57 +0000

    VMOSLIST

---
+++ Get-VM_OS_List/List VM OS's.ps1
@@ -0,0 +1,16 @@
+﻿$VMHost = Read-host "What is the name/ip of the host"
+
+
+Connect-VIServer -server $VMHost
+
+#$Location = Read-Host "What is the name of the container"
+
+$VMs = Get-VM | Sort-Object Name | Select-Object -ExpandProperty Name  | Out-File -FilePath "C:\temp\List.txt"
+$VMList = Get-Content C:\temp\List.txt
+
+Foreach ($VM in $VMlist){
+$VMGuestOS = Get-VMGuest -VM $VM
+Write-host $VMGuestOS | Format-Table
+}
+
+Disconnect-VIserver -force -confirm:$false
